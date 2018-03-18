c02f9e3582b35584ac4169108086337a38ce7763
Tony To <chungtcc@gmail.com>
2018-03-18 18:06:53 +0800

    Add database migration

---
+++ database/migrations/2018_03_18_100007_create_routes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoutesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('token_id')->unsigned();
+            $table->foreign('token_id')
+                ->references('id')->on('tokens')
+                ->onDelete('cascade');
+            $table->integer('total_distance')->nullable();
+            $table->integer('total_time')->nullable();
+            $table->longText('error')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routes');
+    }
+}
---
+++ database/migrations/2018_03_18_100036_create_route_dropoffs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRouteDropoffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('route_dropoffs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('route_id')->unsigned();
+            $table->foreign('route_id')
+                ->references('id')->on('routes')
+                ->onDelete('cascade');
+            $table->integer('step')->nullable();
+            $table->double('latitube', 10, 7);
+            $table->double('longitube', 10, 7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('route_dropoffs');
+    }
+}
