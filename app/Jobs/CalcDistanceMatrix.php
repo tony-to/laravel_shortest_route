684c2728a77e31502ac308fd2cbf8511f0e61431
Tony To <chungtcc@gmail.com>
2018-04-03 00:36:10 +0800

    Use use dropoffs as destination to calculate the shortest route

--- app/Jobs/CalcDistanceMatrix.php
+++ app/Jobs/CalcDistanceMatrix.php
@@ -9,6 +9,8 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
+
 
 class CalcDistanceMatrix implements ShouldQueue
 {
@@ -37,58 +39,95 @@ class CalcDistanceMatrix implements ShouldQueue
 
             //Convert the db data to Google API parameter
             $route_origin = $this->route->start;
-            $route_waypoints = $this->route->dropoffs;
-
+            $route_dropoffs = $this->route->dropoffs;
             $origin_string = $route_origin->latitube.",".$route_origin->longitube;
-            //get the waypoint_order with optimized route
-            $waypoints_string = "optimize:true"; 
 
-            foreach ($route_waypoints as $waypoint) {
-                $waypoints_string .= "|".$waypoint->latitube.",".$waypoint->longitube;
-            }
-
-
-            //When request path(A->B->C->A), path(A->B->C) - path(C->A) will be the shortest path
-            $response_string = \GoogleMaps::load("directions")
-                    ->setParam([
-                                "origin"          => $origin_string, 
-                                "destination"     => $origin_string, 
-                                "waypoints"       => $waypoints_string
-                                ])
-                      ->get();  
+            //Result variable
+            $total_distance = 0;
+            $total_time = 0;
+            $dropoffs_result = array();
 
 
-            $response = json_decode($response_string);
-            if(isset($response->status) && $response->status == "OK"){
-                $total_distance = 0;
-                $total_time = 0;
-                $waypoint_order = $response->routes[0]->waypoint_order;
+            //Use all dropoff points as destination to find the optimize path
+            for($index = 0; $index < sizeof($route_dropoffs); $index++){
+                //Assume one of dropoff is destination
+                $last_dropoff = clone($route_dropoffs[$index]);
+                $last_dropoff->step = (sizeof($route_dropoffs)-1);
+                $dropoff_string = $last_dropoff->latitube.",".$last_dropoff->longitube;
+                
+                //Create temp waypoints and remove destination point
+                $temp_waypoints = clone($route_dropoffs);
+                unset($temp_waypoints[$index]);
 
-                //Update the step ordering of all dropoff points
-                for($i = 0;$i < sizeof($route_waypoints); $i++) {
-                    $route_waypoints[$i]->step = $waypoint_order[$i];
+                //get the waypoint_order with optimized route
+                $waypoints_string = "optimize:true"; 
+                foreach ($temp_waypoints as $dropoff) {
+                    $waypoints_string .= "|".$dropoff->latitube.",".$dropoff->longitube;
                 }
 
-                $legs = $response->routes[0]->legs;
+                //Call Google direction API to check the distance and duration
+                $response_string = \GoogleMaps::load("directions")
+                        ->setParam([
+                                    "origin"          => $origin_string, 
+                                    "destination"     => $dropoff_string, 
+                                    "waypoints"       => $waypoints_string
+                                    ])
+                          ->get();  
 
-                //Calc distance and time (Will not count last leg (C->A))
-                for($i = 0;$i < sizeof($legs) - 1; $i++) {
-                    $total_distance += $legs[$i]->distance->value;
-                    $total_time += $legs[$i]->duration->value;
+                // Log::info($response_string);
+
+                $response = json_decode($response_string);
+                if(isset($response->status) && $response->status == "OK"){
+
+                    $legs = $response->routes[0]->legs;
+
+                    //Calc distance and time
+                    $temp_total_distance = 0;
+                    $temp_total_time = 0;
+
+                    for($i = 0;$i < sizeof($legs); $i++) {
+                        $temp_total_distance += $legs[$i]->distance->value;
+                        $temp_total_time += $legs[$i]->duration->value;
+                    }
+
+                    // Log::info($temp_total_distance);
+                    // Log::info($temp_total_time);
+
+                    //Check the time is shortest
+                    if($total_distance == 0 || $temp_total_distance < $total_distance){
+                        $total_distance = $temp_total_distance;
+                        $total_time = $temp_total_time;
+
+
+                        $waypoint_order = $response->routes[0]->waypoint_order;
+
+                        //Update the step ordering of all dropoff points
+                        $i = 0;
+                        foreach ($temp_waypoints as $temp_waypoint) {
+                            $temp_waypoint->step = $waypoint_order[$i];
+                            $i++;
+                        }
+
+                        $dropoffs_result = clone($temp_waypoints);
+                        $dropoffs_result[] = clone($last_dropoff);
+
+                    }
+
+                }else{
+                    $error_message = "Google API error";
+
+                    if(isset($response->error_message)){
+                        $error_message = $response->error_message;
+                    }
+                    $this->route->update(["status" => Route::FAILURE, "error" => $response->error_message]);   
+                    return false;
                 }
-
-                //Save the path, distance and time and mark as success
-                $this->route->dropoffs()->saveMany($route_waypoints);
-                $this->route->update(["total_distance"=>$total_distance, "total_time"=>$total_time, "status" => Route::SUCCESS]);
-                return true;
             }
-
-            $error_message = "Google API error";
-
-            if(isset($response->error_message)){
-                $error_message = $response->error_message;
-            }
-            $this->route->update(["status" => Route::FAILURE, "error" => $response->error_message]);
+                
+            //Save the path, distance and time and mark as success
+            $this->route->dropoffs()->saveMany($dropoffs_result);
+            $this->route->update(["total_distance"=>$total_distance, "total_time"=>$total_time, "status" => Route::SUCCESS]);
+            return true;
             
         }catch(Exception $e){
             return $e;
